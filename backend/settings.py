fdea8a7fe4bdac66e959328d3aa92a2831412298
unknown <jimoh.iyiola11@gmail.com>
2022-06-27 12:00:17 +0100

    Fixed the quizzes (all) category and edited README.md

--- backend/settings.py
+++ backend/settings.py
@@ -5,3 +5,5 @@ load_dotenv()
 db_name = os.environ.get("database")
 db_user=os.environ.get("user")
 db_password = os.environ.get("password")
+
+#You should rename the environment variables to your machine's environment variables peculiar to your database details. 
